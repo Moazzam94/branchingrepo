6244f65d0eccf0516eb1be8322773312a7efa087
Moazzam94 <moazzamsadiq94@gmail.com>
2020-10-28 21:20:34 +0500

    unique feature of transmission updated

--- garage.java
+++ garage.java
@@ -8,8 +8,8 @@ public class garage {
         car1.make="BMW";
         car1.model="i8";
         car1.size=4;
-        car1.LCD="NO";
-        car1.transmission="Auto";
+        car1.LCD="Yes";
+        car1.transmission="Manual";
         System.out.println(car1.numberofDoors);
         System.out.println(car1.color);
         System.out.println(car1.make);
