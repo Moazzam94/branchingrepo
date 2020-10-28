112e705c3d84216c862b13e907ec7172d3f84a98
Moazzam94 <moazzamsadiq94@gmail.com>
2020-10-28 21:12:46 +0500

    unique feature transmision added

--- garage.java
+++ garage.java
@@ -3,15 +3,19 @@ package com.company;
 public class garage {
     public static void main(String[]args){
         car car1=new car();
-        car1.numberofDoors=2;
-        car1.color="white";
+        car1.numberofDoors=4;
+        car1.color="black";
         car1.make="BMW";
         car1.model="i8";
-        car1.size=2;
+        car1.size=4;
+        car1.LCD="NO";
+        car1.transmission="Auto";
         System.out.println(car1.numberofDoors);
         System.out.println(car1.color);
         System.out.println(car1.make);
         System.out.println(car1.model);
         System.out.println(car1.size);
+        System.out.println(car1.transmission);
+        System.out.println(car1.LCD);
     }
 }
